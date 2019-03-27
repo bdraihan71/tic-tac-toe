024d18172f0ec3e17c064ef913d084391019bc74
Your Name <raihan@techynaf.com>
2019-03-27 18:33:26 +0600

    tic tac toe game version 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      player: "X"
+    };
+  }
+
+  checkWinner() {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let index = 0; index < lines.length; index++) {
+      const [a, b, c] = lines[index];
+      if (
+        this.state.squares[a] &&
+        this.state.squares[a] === this.state.squares[b] &&
+        this.state.squares[a] === this.state.squares[c]
+      ) {
+        if (alert("winner " + this.state.player)) {
+        } else window.location.reload();
+        // console.log(this.state.player+"you win");
+      }
+    }
+  }
+  handleClick(index) {
+    const squares = this.state.squares;
+    if (this.state.squares[index] === null) {
+      squares[index] = this.state.player;
+      const player = this.state.player === "X" ? "0" : "X";
+      this.setState({
+        squares: squares,
+        player: player
+      });
+      this.checkWinner();
+    }
+  }
+  refreshPage() {
+    window.location.reload();
+  }
+
   render() {
+    const Box = this.state.squares.map((box, index) => (
+      <div className="box" key={index} onClick={() => this.handleClick(index)}>
+        {box}
+      </div>
+    ));
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Tic Tac Toe</h1>
+        <h3 type="button" onClick={this.refreshPage}>
+          <span>Reload</span>
+        </h3>
+        <div className="main-box">{Box}</div>
       </div>
     );
   }
